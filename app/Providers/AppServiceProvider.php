0f930cd24ce8fff342baa08d9ffdb5465f3a8029
Igor Santos <igors.d@hotmail.com>
2019-10-06 13:46:52 -0300

    Removed Default String Length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,7 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Schema::defaultStringLength(191);
-        setlocale(LC_TIME, config('app.locale'));
+       //
     }
 }

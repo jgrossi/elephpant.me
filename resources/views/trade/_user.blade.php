a6939417f9ee22b31776ee6a31b99495ccb63d77
Junior Grossi <juniorgro@gmail.com>
2019-10-07 09:18:00 -0300

    chore: add link to user's herd on trading page

--- resources/views/trade/_user.blade.php
+++ resources/views/trade/_user.blade.php
@@ -2,7 +2,9 @@
     <img src="{{ $user->avatar() }}" width="50" alt="" class="img-thumbnail rounded-circle img-fluid mr-3">
     <div class="media-body">
         <p class="mb-0">
-            <strong>{{ $user->name }}</strong>
+            <strong>
+                <a href="{{ route('herds.show', $user->twitter) }}">{{ $user->name }}</a>
+            </strong>
             <span class="ml-2 text-muted"><a href="https://twitter.com/{{ $user->twitter }}">{{ '@' . $user->twitter }}</a> on Twitter</span>
         </p>
         <p class="mb-0">

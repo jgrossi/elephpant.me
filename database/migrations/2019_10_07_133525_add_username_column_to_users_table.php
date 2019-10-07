d663bd9daf9b9e18febe14fa5d43b9c456f73762
Junior Grossi <juniorgro@gmail.com>
2019-10-07 10:49:32 -0300

    chore: make username unique

--- database/migrations/2019_10_07_133525_add_username_column_to_users_table.php
+++ database/migrations/2019_10_07_133525_add_username_column_to_users_table.php
@@ -9,7 +9,7 @@ class AddUsernameColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('username')->after('twitter');
+            $table->string('username')->unique()->after('twitter');
         });
     }
 

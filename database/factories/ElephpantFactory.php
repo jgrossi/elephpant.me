142ad9cc2025aa62b93ea9c26020d88cf63a7e3b
Sven Luijten <svenluijten1995@gmail.com>
2019-10-08 18:06:13 +0200

    tests: initial testing setup

--- database/factories/ElephpantFactory.php
+++ database/factories/ElephpantFactory.php
@@ -8,9 +8,9 @@ use Faker\Generator as Faker;
 $factory->define(Elephpant::class, function (Faker $faker) {
     return [
         'name' => $faker->firstName,
-        'popular_name' => $faker->name,
-        'year' => (int)$faker->dateTimeBetween('-12 years', 'now')->format('Y'),
+        'year' => (int) $faker->dateTimeBetween('-12 years', 'now')->format('Y'),
         'sponsor' => $faker->company,
         'image' => $faker->imageUrl(),
+        'description' => $faker->text,
     ];
 });

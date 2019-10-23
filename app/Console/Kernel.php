d88e1b547d50f493850f14f688abdc73b899cac0
Junior Grossi <juniorgro@gmail.com>
2019-10-23 09:43:23 -0300

    style: fix style to PSR2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('elephpants:read')
-                  ->twiceDaily();
+        $schedule->command('elephpants:read')
+            ->twiceDaily();
     }
 
     /**

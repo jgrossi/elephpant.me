012199c545841f3333578e2cb2c05db2fbaa7d0d
Pierre Grimaud <grimaud.pierre@gmail.com>
2020-01-31 12:37:07 +0100

    Update statistics page

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -10,7 +10,7 @@ class StatisticsController extends Controller
     public function index()
     {
         $elephpants = DB::table('elephpants')
-            ->select(DB::raw('COUNT(elephpant_user.elephpant_id) as nbElephpant, id, name, description'))
+            ->select(DB::raw('COUNT(elephpant_user.elephpant_id) as nbElephpant, name, description, SUM(elephpant_user.quantity) as totalElephpant'))
             ->leftJoin('elephpant_user', 'elephpants.id', '=', 'elephpant_user.elephpant_id')
             ->orderBy('nbElephpant', 'desc')
             ->orderBy('elephpants.id', 'desc')
--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -12,16 +12,17 @@
                 </div>
                 <div class="card">
                     <div class="card-header">
-                        <strong>Ownership of elephpants</strong>
+                        <strong>Ownership of elePHPants</strong>
                     </div>
                     <div class="card-body">
                         <table class="table table-striped table-bordered mb-0">
                             <thead>
                             <tr>
-                                <th scope="col" width="7%" class="text-center">#</th>
-                                <th scope="col" width="35%">Name</th>
-                                <th scope="col" width="12%" class="text-center">Ownership percent</th>
-                                <th scope="col" width="12%" class="text-center">Number</th>
+                                <th scope="col" width="7%" class="text-center align-middle">#</th>
+                                <th scope="col" width="48%" class=" align-middle">Name</th>
+                                <th scope="col" width="15%" class="text-center align-middle">Ownership percent</th>
+                                <th scope="col" width="15%" class="text-center align-middle">Ownership users</th>
+                                <th scope="col" width="15%" class="text-center align-middle">Total</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -29,11 +30,12 @@
                                 <tr>
                                     <td class="text-center align-middle">{{ $key + 1 }}</td>
                                     <td>
-                                        <span class="font-weight-bold">#{{ $elephpant->id }} - {{ $elephpant->name }}</span><br/>
-                                        {{ $elephpant->description }}
+                                        <span class="font-weight-bold">{{ $elephpant->name }}</span>
+                                        <span>- {{ $elephpant->description }}</span>
                                     </td>
                                     <td class="text-center align-middle">{{ round((($elephpant->nbElephpant/$nbUsers) * 100), 2) }}%</td>
                                     <td class="text-center align-middle">{{ $elephpant->nbElephpant }}</td>
+                                    <td class="text-center align-middle">{{ (int)$elephpant->totalElephpant }}</td>
                                 </tr>
                             @endforeach
                             </tbody>

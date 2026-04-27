fb0353dae939d52a35995bf105b663bb535108fa
Kasper Hartwich <kasperhartwich@gmail.com>
2026-04-27 13:55:52 +0200

    Add filter for unique/double elephpants on herd show page

--- resources/views/herd/_stats.blade.php
+++ resources/views/herd/_stats.blade.php
@@ -1,26 +1,26 @@
+@php
+    $isShow = request()->routeIs('herds.show');
+    $boxes = [
+        ['label' => 'Unique', 'value' => $stats['unique'], 'filter' => 'unique'],
+        ['label' => 'Double', 'value' => $stats['double'], 'filter' => 'double'],
+        ['label' => 'Total', 'value' => $stats['total'], 'filter' => 'total'],
+    ];
+@endphp
 <div id="stats-content" class="row mt-4">
-    <div class="col-12 col-md-4 mb-2 mb-md-0">
-        <div class="jumbotron py-2 mb-0">
-            <div class="container">
-                <p class="lead mb-1">Unique</p>
-                <h1 class="display-5 mb-0">{{ $stats['unique'] }}</h1>
+    @foreach($boxes as $box)
+        <div class="col-12 col-md-4 mb-2 mb-md-0">
+            @if($isShow)
+                <a href="{{ url()->current() . ($box['filter'] === 'total' ? '' : '?filter=' . $box['filter']) }}" class="text-decoration-none text-reset">
+            @endif
+            <div class="jumbotron py-2 mb-0">
+                <div class="container">
+                    <p class="lead mb-1">{{ $box['label'] }}</p>
+                    <h1 class="display-5 mb-0">{{ $box['value'] }}</h1>
+                </div>
             </div>
+            @if($isShow)
+                </a>
+            @endif
         </div>
-    </div>
-    <div class="col-12 col-md-4 mb-2 mb-md-0">
-        <div class="jumbotron py-2 mb-0">
-            <div class="container">
-                <p class="lead mb-1">Double</p>
-                <h1 class="display-5 mb-0">{{ $stats['double'] }}</h1>
-            </div>
-        </div>
-    </div>
-    <div class="col-12 col-md-4 mb-2 mb-md-0">
-        <div class="jumbotron py-2 mb-0">
-            <div class="container">
-                <p class="lead mb-1">Total</p>
-                <h1 class="display-5 mb-0">{{ $stats['total'] }}</h1>
-            </div>
-        </div>
-    </div>
+    @endforeach
 </div>

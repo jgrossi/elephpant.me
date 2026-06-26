f25c01d3576dada909a2dfdaabde282057f9ff11
teiling88 <teiling88@gmail.com>
2026-06-26 17:15:09 +0200

    #254 add /api/herd/{username} Endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\HerdController;
 use Illuminate\Http\Request;
 
 /*
@@ -16,3 +17,5 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/herd/{username}', [HerdController::class, 'show'])->name('api.herds.show');
